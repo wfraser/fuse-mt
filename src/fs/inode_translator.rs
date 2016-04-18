d6a9ac565c4eb5e3836fe42687058f867d1c1fcf
William R. Fraser <wfraser@codewise.org>
2016-04-18 07:19:04 -0700

    reorder and add comments so that we match rust-fuse documentation

--- src/fs/inode_translator.rs
+++ src/fs/inode_translator.rs
@@ -34,31 +34,33 @@ pub trait PathFilesystem {
         // Nothing.
     }
 
-    fn getattr(&mut self, _req: &Request, _path: &Path) -> ResultGetattr {
-        Err(libc::ENOSYS)
-    }
-
     fn lookup(&mut self, _req: &Request, _parent: &Path, _name: &Path) -> ResultLookup {
         Err(libc::ENOSYS)
     }
 
-    fn opendir(&mut self, _req: &Request, _path: &Path, _flags: u32) -> ResultOpen {
+    fn getattr(&mut self, _req: &Request, _path: &Path) -> ResultGetattr {
         Err(libc::ENOSYS)
     }
 
-    fn releasedir(&mut self, _req: &Request, _path: &Path, _fh: u64, _flags: u32) -> ResultEmpty {
-        Err(libc::ENOSYS)
-    }
+    // setattr
 
-    fn readdir(&mut self, _req: &Request, _path: &Path, _fh: u64, _offset: u64) -> ResultReaddir {
-        Err(libc::ENOSYS)
-    }
+    // readlink
+
+    // mknod
+
+    // mkdir
+
+    // unlink
+
+    // rmdir
+
+    // symlink
+
+    // rename
+
+    // link
 
     fn open(&mut self, _req: &Request, _path: &Path, _flags: u32) -> ResultOpen {
-        Err(libc::ENOSYS)
-    }
-
-    fn release(&mut self, _req: &Request, _path: &Path, _fh: u64, _flags: u32, _lock_owner: u64, _flush: bool) -> ResultEmpty {
         Err(libc::ENOSYS)
     }
 
@@ -74,9 +76,47 @@ pub trait PathFilesystem {
         Err(libc::ENOSYS)
     }
 
+    fn release(&mut self, _req: &Request, _path: &Path, _fh: u64, _flags: u32, _lock_owner: u64, _flush: bool) -> ResultEmpty {
+        Err(libc::ENOSYS)
+    }
+
     fn fsync(&mut self, _req: &Request, _path: &Path, _fh: u64, _datasync: bool) -> ResultEmpty {
         Err(libc::ENOSYS)
     }
+
+    fn opendir(&mut self, _req: &Request, _path: &Path, _flags: u32) -> ResultOpen {
+        Err(libc::ENOSYS)
+    }
+
+    fn readdir(&mut self, _req: &Request, _path: &Path, _fh: u64, _offset: u64) -> ResultReaddir {
+        Err(libc::ENOSYS)
+    }
+
+    fn releasedir(&mut self, _req: &Request, _path: &Path, _fh: u64, _flags: u32) -> ResultEmpty {
+        Err(libc::ENOSYS)
+    }
+
+    // fsyncdir
+
+    // statfs
+
+    // setxattr
+
+    // getxattr
+
+    // listxattr
+
+    // removexattr
+
+    // access
+
+    // create
+
+    // getlk
+
+    // setlk
+
+    // bmap
 }
 
 pub struct InodeTranslator<T> {
@@ -117,15 +157,6 @@ impl<T: PathFilesystem> Filesystem for InodeTranslator<T> {
         self.target.destroy(req);
     }
 
-    fn getattr(&mut self, req: &Request, ino: u64, reply: ReplyAttr) {
-        let path = get_path!(self, ino, reply);
-        debug!("getattr: {:?}", path);
-        match self.target.getattr(req, &path) {
-            Ok((ref ttl, ref attr)) => reply.attr(ttl, attr),
-            Err(e) => reply.error(e),
-        }
-    }
-
     fn lookup(&mut self, req: &Request, parent: u64, name: &Path, reply: ReplyEntry) {
         let parent_path = get_path!(self, parent, reply);
         debug!("lookup: {:?}, {:?}", parent_path, name);
@@ -140,20 +171,110 @@ impl<T: PathFilesystem> Filesystem for InodeTranslator<T> {
         }
     }
 
-    fn opendir(&mut self, req: &Request, ino: u64, flags: u32, reply: ReplyOpen) {
+    // fn forget(&mut self, _req: &Request, _ino: u64, _nlookup: u64)
+
+    fn getattr(&mut self, req: &Request, ino: u64, reply: ReplyAttr) {
         let path = get_path!(self, ino, reply);
-        debug!("opendir: {:?}", path);
-        match self.target.opendir(req, &path, flags) {
+        debug!("getattr: {:?}", path);
+        match self.target.getattr(req, &path) {
+            Ok((ref ttl, ref attr)) => reply.attr(ttl, attr),
+            Err(e) => reply.error(e),
+        }
+    }
+
+    /*
+    fn setattr(&mut self,
+               req: &Request,
+               ino: u64,
+               mode: Option<u32>,           // chmod
+               uid: Option<u32>,            // chown
+               gid: Option<u32>,            // chown
+               size: Option<u64>,           // truncate
+               atime: Option<Timespec>,     // utimens
+               mtime: Option<Timespec>,     // utimens
+               fh: Option<u64>,             // ?
+               crtime: Option<Timespec>,    // ?
+               chgtime: Option<Timespec>,   // ?
+               bkuptime: Option<Timespec>,  // ?
+               flags: Option<u32>,
+               reply: ReplyAttr)
+    */
+
+    // readlink
+
+    // mknod
+
+    // mkdir
+
+    // unlink
+
+    // rmdir
+
+    // symlink
+
+    // rename
+
+    // link
+
+    fn open(&mut self, req: &Request, ino: u64, flags: u32, reply: ReplyOpen) {
+        let path = get_path!(self, ino, reply);
+        debug!("open: {:?}", path);
+        match self.target.open(req, &path, flags) {
             Ok((fh, flags)) => reply.opened(fh, flags),
             Err(e) => reply.error(e),
         }
     }
 
-    fn releasedir(&mut self, req: &Request, ino: u64, fh: u64, flags: u32, reply: ReplyEmpty) {
+    fn read(&mut self, req: &Request, ino: u64, fh: u64, offset: u64, size: u32, reply: ReplyData) {
         let path = get_path!(self, ino, reply);
-        debug!("releasedir: {:?}", path);
-        match self.target.releasedir(req, &path, fh, flags) {
+        debug!("read: {:?} {:#x} @ {:#x}", path, size, offset);
+        match self.target.read(req, &path, fh, offset, size) {
+            Ok(ref data) => reply.data(data),
+            Err(e) => reply.error(e),
+        }
+    }
+
+    fn write(&mut self, req: &Request, ino: u64, fh: u64, offset: u64, data: &[u8], flags: u32, reply: ReplyWrite) {
+        let path = get_path!(self, ino, reply);
+        debug!("write: {:?} {:#x} @ {:#x}", path, data.len(), offset);
+        match self.target.write(req, &path, fh, offset, data, flags) {
+            Ok(written) => reply.written(written),
+            Err(e) => reply.error(e),
+        }
+    }
+
+    fn flush(&mut self, req: &Request, ino: u64, fh: u64, lock_owner: u64, reply: ReplyEmpty) {
+        let path = get_path!(self, ino, reply);
+        debug!("flush: {:?}", path);
+        match self.target.flush(req, &path, fh, lock_owner) {
             Ok(()) => reply.ok(),
+            Err(e) => reply.error(e),
+        }
+    }
+
+    fn release(&mut self, req: &Request, ino: u64, fh: u64, flags: u32, lock_owner: u64, flush: bool, reply: ReplyEmpty) {
+        let path = get_path!(self, ino, reply);
+        debug!("release: {:?}", path);
+        match self.target.release(req, &path, fh, flags, lock_owner, flush) {
+            Ok(()) => reply.ok(),
+            Err(e) => reply.error(e),
+        }
+    }
+
+    fn fsync(&mut self, req: &Request, ino: u64, fh: u64, datasync: bool, reply: ReplyEmpty) {
+        let path = get_path!(self, ino, reply);
+        debug!("fsync: {:?}", path);
+        match self.target.fsync(req, &path, fh, datasync) {
+            Ok(()) => reply.ok(),
+            Err(e) => reply.error(e),
+        }
+    }
+
+    fn opendir(&mut self, req: &Request, ino: u64, flags: u32, reply: ReplyOpen) {
+        let path = get_path!(self, ino, reply);
+        debug!("opendir: {:?}", path);
+        match self.target.opendir(req, &path, flags) {
+            Ok((fh, flags)) => reply.opened(fh, flags),
             Err(e) => reply.error(e),
         }
     }
@@ -208,57 +329,34 @@ impl<T: PathFilesystem> Filesystem for InodeTranslator<T> {
         }
     }
 
-    fn open(&mut self, req: &Request, ino: u64, flags: u32, reply: ReplyOpen) {
+    fn releasedir(&mut self, req: &Request, ino: u64, fh: u64, flags: u32, reply: ReplyEmpty) {
         let path = get_path!(self, ino, reply);
-        debug!("open: {:?}", path);
-        match self.target.open(req, &path, flags) {
-            Ok((fh, flags)) => reply.opened(fh, flags),
-            Err(e) => reply.error(e),
-        }
-    }
-
-    fn release(&mut self, req: &Request, ino: u64, fh: u64, flags: u32, lock_owner: u64, flush: bool, reply: ReplyEmpty) {
-        let path = get_path!(self, ino, reply);
-        debug!("release: {:?}", path);
-        match self.target.release(req, &path, fh, flags, lock_owner, flush) {
+        debug!("releasedir: {:?}", path);
+        match self.target.releasedir(req, &path, fh, flags) {
             Ok(()) => reply.ok(),
             Err(e) => reply.error(e),
         }
     }
 
-    fn read(&mut self, req: &Request, ino: u64, fh: u64, offset: u64, size: u32, reply: ReplyData) {
-        let path = get_path!(self, ino, reply);
-        debug!("read: {:?} {:#x} @ {:#x}", path, size, offset);
-        match self.target.read(req, &path, fh, offset, size) {
-            Ok(ref data) => reply.data(data),
-            Err(e) => reply.error(e),
-        }
-    }
+    // fsyncdir
 
-    fn write(&mut self, req: &Request, ino: u64, fh: u64, offset: u64, data: &[u8], flags: u32, reply: ReplyWrite) {
-        let path = get_path!(self, ino, reply);
-        debug!("write: {:?} {:#x} @ {:#x}", path, data.len(), offset);
-        match self.target.write(req, &path, fh, offset, data, flags) {
-            Ok(written) => reply.written(written),
-            Err(e) => reply.error(e),
-        }
-    }
+    // statfs
 
-    fn flush(&mut self, req: &Request, ino: u64, fh: u64, lock_owner: u64, reply: ReplyEmpty) {
-        let path = get_path!(self, ino, reply);
-        debug!("flush: {:?}", path);
-        match self.target.flush(req, &path, fh, lock_owner) {
-            Ok(()) => reply.ok(),
-            Err(e) => reply.error(e),
-        }
-    }
+    // setxattr
 
-    fn fsync(&mut self, req: &Request, ino: u64, fh: u64, datasync: bool, reply: ReplyEmpty) {
-        let path = get_path!(self, ino, reply);
-        debug!("fsync: {:?}", path);
-        match self.target.fsync(req, &path, fh, datasync) {
-            Ok(()) => reply.ok(),
-            Err(e) => reply.error(e),
-        }
-    }
+    // getxattr
+
+    // listxattr
+
+    // removexattr
+
+    // access
+
+    // create
+
+    // getlk
+
+    // setlk
+
+    // bmap
 }
