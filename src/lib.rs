98e879c98610d9427df37fa63305d39be1f9d512
William R. Fraser <wfraser@codewise.org>
2017-01-08 23:13:15 -0800

    correct the fuse install commands for MacOS

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! FuseMT -- A higher-level FUSE (Filesystem in Userspace) interface and wrapper around the
+//! low-level `rust-fuse` library that makes implementing a filesystem a bit easier.
+//!
+//! FuseMT translates inodes to paths and dispatches I/O operations to multiple threads, and
+//! simplifies some details of filesystem implementation, for example: splitting the `setattr` call
+//! into multiple separate operations, and simplifying the `readdir` call so that filesystems don't
+//! need to deal with pagination.
+//!
+//! To implement a filesystem, implement the `FilesystemMT` trait. Not all functions in it need to
+//! be implemented -- the default behavior is to return `ENOSYS` ("Function not implemented"). For
+//! example, a read-only filesystem can skip implementing the `write` call and many others.
+
 extern crate fuse;
 extern crate libc;
 extern crate threadpool;
@@ -10,5 +22,6 @@ mod directory_cache;
 mod fusemt;
 mod inode_table;
 
-pub use fuse::*;
+pub use fuse::{FileAttr, FileType, mount, spawn_mount};
+
 pub use fusemt::*;
