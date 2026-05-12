121f0a876bbfb581fd25da678cbbbeb1a386d152
Bill Fraser <bill@wfraser.dev>
2026-05-12 11:39:52 -0700

    update to fuser-0.17 mount functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,8 @@
 //! example, a read-only filesystem can skip implementing the `write` call and many others.
 
 //
-// Copyright (c) 2016-2022 by William R. Fraser
+// Copyright (c) 2016-2026 by William R. Fraser
 //
-
-#![deny(rust_2018_idioms)]
 
 #[macro_use]
 extern crate log;
@@ -26,40 +24,52 @@ mod types;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-pub use fuser::FileType;
 pub use crate::fusemt::*;
 pub use crate::types::*;
+pub use fuser::FileType;
 
 // Forward to similarly-named fuser functions to work around deprecation for now.
 // When these are removed, we'll have to either reimplement or break reverse compat.
 // Keep the doc comments in sync with those in fuser.
 
+use fuser::MountOption;
 use std::ffi::OsStr;
 use std::io;
 use std::path::Path;
 
 /// Mount the given filesystem to the given mountpoint. This function will not return until the
 /// filesystem is unmounted.
-#[inline(always)]
 pub fn mount<FS: fuser::Filesystem, P: AsRef<Path>>(
     fs: FS,
     mountpoint: P,
     options: &[&OsStr],
 ) -> io::Result<()> {
-    #[allow(deprecated)]
-    fuser::mount(fs, mountpoint, options)
+    fuser::mount2(fs, mountpoint, &make_mount_options(options)?)
 }
 
 /// Mount the given filesystem to the given mountpoint. This function spawns a background thread to
 /// handle filesystem operations while being mounted and therefore returns immediately. The
 /// returned handle should be stored to reference the mounted filesystem. If it's dropped, the
 /// filesystem will be unmounted.
-#[inline(always)]
 pub fn spawn_mount<FS: fuser::Filesystem + Send + 'static, P: AsRef<Path>>(
     fs: FS,
     mountpoint: P,
     options: &[&OsStr],
 ) -> io::Result<fuser::BackgroundSession> {
-    #[allow(deprecated)]
-    fuser::spawn_mount(fs, mountpoint, options)
+    fuser::spawn_mount2(fs, mountpoint, &make_mount_options(options)?)
+}
+
+fn make_mount_options(options: &[&OsStr]) -> io::Result<fuser::Config> {
+    let mut cfg = fuser::Config::default();
+    for opt in options {
+        let opt_str = opt.to_str().ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidData,
+                "non-utf8 mount option not supported",
+            )
+        })?;
+        cfg.mount_options
+            .push(MountOption::CUSTOM(opt_str.to_owned()));
+    }
+    Ok(cfg)
 }
